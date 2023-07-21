c6e3aa8b3814117e266dea078c1285c3c3cf6727
eSuardOdin <esuardodin@gmail.com>
2023-07-21 05:03:09 +0200

    Can add a user to db

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿string dbPath = "bank_app.db.sqlite";
-var dbManager = new Db.DbManager(dbPath);
+﻿using Entities;
+var dbManager = new Db.DbManager();
+
+Console.WriteLine("add a user : ");
+Console.Write("username : ");
+string log = Console.ReadLine();
+Console.Write("password : ");
+string pwd = Console.ReadLine();
+
+Utilisateur ut = new Utilisateur(0, log, pwd);
+ut.AddUtilisateurToDb();
+
--- classes/Utilisateur.cs
+++ classes/Utilisateur.cs
@@ -1,13 +1,24 @@
 namespace Entities {
+    using Db;
     public class Utilisateur 
     {
         private int IdUser {get; init;}
         private string LoginUser {get; init;}
-
         private string PwdUser {get; init;}
 
-        public Utilisateur(int idUser, Db.DbManager dbManager) 
+        public Utilisateur(int idUser, string loginUser, string passwordUser) 
         {
+            IdUser = idUser;
+            LoginUser = loginUser;
+            PwdUser = passwordUser;
+        }
+
+        public void AddUtilisateurToDb()
+        {
+            var dbMan = new DbManager();
+            Console.WriteLine($"INSERT INTO Utilisateurs (login_user, pwd_user) VALUES ({this.LoginUser}, {this.PwdUser});");
+            dbMan.ExecuteNonQuery($"INSERT INTO Utilisateurs (login_user, pwd_user) VALUES (\"{this.LoginUser}\", \"{this.PwdUser}\");");
+
             
         }
     }    
